1dd39e82220c515086cb894653ceb880a8849ef5
CAMARA Diangou <diangou.cmr09@gmail.com>
2025-11-05 09:07:06 +0100

    Initialisation

---
+++ WhatToWatch/WhatToWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatToWatchApp.swift
+//  WhatToWatch
+//
+//  Created by CAMARA Diangou on 05/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct WhatToWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
