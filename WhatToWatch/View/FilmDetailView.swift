c4ad12fae4db299693729102de1f6befaaca93d3
KOKONYANGE-NKASEI Laura <lk253768@dvqncg77k0.edu.atlas.local>
2025-11-05 10:54:56 +0100

    Ajout fake data + affichage dans vue

--- WhatToWatch/View/FilmDetailView.swift
+++ WhatToWatch/View/FilmDetailView.swift
@@ -5,3 +5,49 @@
 //  Created by CAMARA Diangou on 05/11/2025.
 //
 
+import SwiftUI
+
+struct FilmDetailView: View {
+    let film: Film
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 16) {
+                AsyncImage(url: URL(string: film.afficheURL)) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                } placeholder: {
+                    Color.gray.opacity(0.3)
+                        .frame(height: 300)
+                }
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(film.titre)
+                        .font(.largeTitle)
+                        .bold()
+                 
+                    HStack {
+                        Text(String(film.note))
+                        Text(String(film.duree))
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    
+                    
+                    
+                    Divider()
+                    
+                    Text(film.description)
+                        .font(.body)
+                }
+                .padding()
+            }
+        }
+        .navigationTitle(film.titre)
+    }
+}
+
+#Preview {
+    FilmDetailView(film: FilmRepository.shared.loadFilms().first!)
+}
--- WhatToWatch/WhatToWatchApp.swift
+++ WhatToWatch/WhatToWatchApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct WhatToWatchApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            FilmView()
         }
     }
 }
